f1a5a7e0cfcc6a08d54ef358c29018c2706d1886
qian <qianwu@thoughtworks.com>
2021-07-03 13:03:51 +0800

    [qian]refactor:  add test for Backstage

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -14,4 +14,16 @@ class GildedRoseTest {
         assertEquals("foo", app.items[0].name);
     }
 
+    @Test
+    void should_double_increase_backstage_pass_value_when_getting_close_to_expiration() {
+
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 11, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        assertEquals(21, app.items[0].quality);
+        app.updateQuality();
+        assertEquals(23, app.items[0].quality);
+    }
+
 }
